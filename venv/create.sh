dfdeaea8099f83cdbfa8a2f676a2dfe7f35b53c4
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-02-23 09:25:56 -0600

    update create

--- venv/create.sh
+++ venv/create.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NAME=coffeaenv
 LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc9-opt
-
 source $LCG/setup.sh
+
 # following https://aarongorka.com/blog/portable-virtualenv/, an alternative is https://github.com/pantsbuild/pex
 python -m venv --copies $NAME
 source $NAME/bin/activate
@@ -19,5 +19,4 @@ sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" 
 sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
 sed -i "3a export PYTHONPATH=${LOCALPATH}:\$PYTHONPATH" $NAME/bin/activate
 
-pip install -e boostedhiggs
 tar -zcf ${NAME}.tar.gz ${NAME}
