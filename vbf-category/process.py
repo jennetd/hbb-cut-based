ae0906df1364ac8961b24cf6752ac444f42b95c3
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-02-25 14:16:56 -0600

    update to local xrd

--- vbf-category/process.py
+++ vbf-category/process.py
@@ -23,11 +23,13 @@ def main():
     from coffea import processor, util, hist
     from boostedhiggs import VBFProcessor
 
+    uproot.open.defaults["xrootd_handler"] = uproot.source.xrootd.MultithreadedXRootDSource
+
     p = VBFProcessor(year=year)
     args = {'schema': NanoAODSchema, 'workers': 4}
  
     this_file = 'infiles/'+str(year)+'_'+str(index)+'.json'
-    out = processor.run_uproot_job(this_file, 'Events', p, processor.futures_executor, args, chunksize=10000)
+    out = processor.run_uproot_job(this_file, 'Events', p, processor.iterative_executor, args, chunksize=10000)
 
     outfile = 'outfiles/'+str(year)+'_'+str(index)+'.coffea'
     util.save(out, outfile)
