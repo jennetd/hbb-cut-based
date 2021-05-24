ad68efc4037e8953ca5af9cb6ea0b3a22fea13aa
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-05-24 13:42:15 -0500

    update vbf submit dirs

--- vbf-category/vbf-bsm/submit.templ.sh
+++ vbf-category/vbf-bsm/submit.templ.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# make sure this is installed
-python3 -m pip install correctionlib==2.0.0rc6
-
 # make dir for input/output
 mkdir infiles-split
 xrdcp root://cmseos.fnal.gov/EOSIN/YEAR_SAMPLE.json infiles-split
