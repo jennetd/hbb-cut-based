01d48e1e8b9a41347cd8dc200400d0b089dacaba
Jennet Dickinson <dickinson.jennet@gmail.com>
2021-08-27 08:00:09 -0500

    fix 2mjj-theory

--- vbf-category/ggf-vbf-ddb2/make-hists-2mjj-theory.py
+++ vbf-category/ggf-vbf-ddb2/make-hists-2mjj-theory.py
@@ -15,7 +15,7 @@ lumis['2017'] = 41.5
 lumis['2018'] = 59.2
 
 systematics = [
-    'PS_weightUp','PS_weightDown',
+    'UEPS_ISRUp','UEPS_ISRDown','UEPS_FSRUp','UEPS_FSRDown',
     'PDF_weightUp','PDF_weightDown',
     'scalevar_7ptUp','scalevar_7ptDown','scalevar_3ptUp','scalevar_3ptDown'
     ]
@@ -62,10 +62,10 @@ def main():
     # Read the histogram from the pickle file
     ggf = pickle.load(open(picklename,'rb')).integrate('region','signal-ggf')
     vbf = pickle.load(open(picklename,'rb')).integrate('region','signal-vbf')
-    
-    if os.path.isfile(year+'/2mjj-signalregion.root'):
-        os.remove(year+'/2mjj-signalregion.root')
-    fout = uproot3.create(year+'/2mjj-signalregion.root')
+    mucr = pickle.load(open(picklename,'rb')).integrate('region','muoncontrol')     
+    if os.path.isfile(year+'/2mjj-signalregion-th.root'):
+        os.remove(year+'/2mjj-signalregion-th.root')
+    fout = uproot3.create(year+'/2mjj-signalregion-th.root')
 
     mc = ['QCD','ttbar','singlet','VV','ggF','VBF','WH','ZH','ttH']
 
@@ -85,7 +85,12 @@ def main():
         for p in ['Wjets','Zjets']:
             print(p)
 
-            for s in systematics:
+            if 'W' in p:
+                systematics_boson = systematics + systematics_Wjets
+            if 'Z' in p:
+                systematics_boson = systematics + systematics_Zjets
+
+            for s in systematics_boson:
                 h = vbf.sum('pt1').integrate('genflavor',int_range=slice(3,4)).integrate('systematic',s).integrate('mjj',int_range=slice(mjjbins[i],mjjbins[i+1])).integrate('ddb1',int_range=slice(ddbthr,1)).integrate('process',p)
                 fout["vbf_pass_mjj"+str(i+1)+"_"+p+"bb_"+s] = hist.export1d(h)
                 h = vbf.sum('pt1').integrate('genflavor',int_range=slice(3,4)).integrate('systematic',s).integrate('mjj',int_range=slice(mjjbins[i],mjjbins[i+1])).integrate('ddb1',int_range=slice(0,ddbthr)).integrate('process',p)
@@ -95,6 +100,40 @@ def main():
                 fout["vbf_pass_mjj"+str(i+1)+"_"+p+"_"+s] = hist.export1d(h)
                 h = vbf.sum('pt1').integrate('genflavor',int_range=slice(1,3)).integrate('systematic',s).integrate('mjj',int_range=slice(mjjbins[i],mjjbins[i+1])).integrate('ddb1',int_range=slice(0,ddbthr)).integrate('process',p)
                 fout["vbf_fail_mjj"+str(i+1)+"_"+p+"_"+s] = hist.export1d(h)
+
+    print("MUON CR")
+    if os.path.isfile(year+'/muonCR-th.root'):
+        os.remove(year+'/muonCR-th.root')
+    fout = uproot3.create(year+'/muonCR-th.root')
+
+    for p in mc:
+        print(p)
+        for s in systematics:
+            h = mucr.integrate('systematic',s).sum('pt1','mjj','genflavor').integrate('ddb1',int_range=slice(ddbthr,1)).integrate('process',p)
+            fout["pass_"+p+"_"+s] = hist.export1d(h)
+            h = mucr.integrate('systematic',s).sum('pt1','mjj','genflavor').integrate('ddb1',int_range=slice(0,ddbthr)).integrate('process',p)
+            fout["fail_"+p+"_"+s] = hist.export1d(h)
+
+    for p in ['Wjets','Zjets']:
+        print(p)
+
+        if 'W' in p:
+            systematics_boson = systematics + systematics_Wjets
+        if 'Z' in p:
+            systematics_boson = systematics + systematics_Zjets
+
+        for s in systematics_boson:
+            h = mucr.integrate('systematic',s).sum('pt1','mjj').integrate('genflavor',int_range=slice(3,4)).integrate('ddb1',int_range=slice(ddbthr,1)).integrate('process',p)
+            fout["pass_"+p+"bb_"+s] = hist.export1d(h)
+            h = mucr.integrate('systematic',s).sum('pt1','mjj').integrate('genflavor',int_range=slice(3,4)).integrate('ddb1',int_range=slice(0,ddbthr)).integrate('process',p)
+            fout["fail_"+p+"bb_"+s] = hist.export1d(h)
+
+            h = mucr.integrate('systematic',s).sum('pt1','mjj').integrate('genflavor',int_range=slice(1,3)).integrate('ddb1',int_range=slice(ddbthr,1)).integrate('process',p)
+            fout["pass_"+p+"_"+s] = hist.export1d(h)
+            h = mucr.integrate('systematic',s).sum('pt1','mjj').integrate('genflavor',int_range=slice(1,3)).integrate('ddb1',int_range=slice(0,ddbthr)).integrate('process',p)
+            fout["fail_"+p+"_"+s] = hist.export1d(h)
+
+    fout.close()
 
     return
 
